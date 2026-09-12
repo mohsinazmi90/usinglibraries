9467741938fdafae25c18555be37debf1ae6b5e4
mohsinazmi90 <mohsin.azmi1990@gmail.com>
2026-09-12 09:38:50 +0500

    add employee files

--- Python Projects/sdet/employee.py
+++ Python Projects/sdet/employee.py
@@ -1,10 +1,18 @@
 # Python Object-Oriented Programming
 
 class Employee:
-    pass
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = (first + "." + last + "@company.com").lower()
+    
+    def fullname(self):
+        return f"{self.last}, {self.first}"
 
-employee1 = Employee()
-employee2 = Employee()
+        
 
-print(employee1)
-print(employee2)
+employee1 = Employee("Mohsin", "Azmi", 50000)
+employee2 = Employee("test", "user", 60000)
+
+print(employee1.fullname())
